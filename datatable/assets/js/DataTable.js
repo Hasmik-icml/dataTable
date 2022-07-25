f71aaf9853a5aa4f26b4f6ea28cf3a925d729974
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-07-25 14:17:56 +0400

    adding pagination

--- datatable/assets/js/DataTable.js
+++ datatable/assets/js/DataTable.js
@@ -5,6 +5,8 @@ class DataTable {
     }
 
     createTable(){
+        const dataCount = Math.ceil(this.data.length/3);
+        this.dataCount = dataCount;
         const $table = document.createElement('table');
         this.$table = $table;
         const $dataTableContainer = document.querySelector('.data-table-container');
@@ -12,8 +14,8 @@ class DataTable {
 
         this.createThead();
         this.createTbody();
-        this.renderData();
-        this.createTfooter();
+        this.renderData(dataCount);
+        this.createTfooter();   
     }
 
     createThead(){
@@ -27,7 +29,6 @@ class DataTable {
         });
 
         $thead.appendChild($tr);
-
         this.$table.appendChild($thead);
     }
 
@@ -37,8 +38,11 @@ class DataTable {
         this.$table.appendChild($tbody);
     }
 
-    renderData(){
-        this.data.map((item) => {
+    renderData(dataCount, rData){
+ 
+        if (!rData) rData = this.data;
+        rData.every((item, index) => {
+            if(index === dataCount) return;
             const $tr = document.createElement('tr');
 
             for (const key in item){
@@ -49,26 +53,37 @@ class DataTable {
 
             this.$tbody.appendChild($tr);
             return $tr;
-        })
+        });
     }
 
     createTfooter(){
         const $tfooter = document.createElement('tr');
+        const $td = document.createElement('td');
 
-        this.data.forEach(item => {
-            const $td = document.createElement('td');
-           
-            const attr = document.createAttribute("colspan").value = '3';
-            $td.setAttributeNode(attr);
+        const attr = document.createAttribute("colspan");     
+        attr.value = "3";
+        $td.setAttributeNode(attr);
 
+        for (let btnCount = 1; btnCount <= this.dataCount; btnCount++){
             const $btn = document.createElement('button');
-            $btn.innerHTML = item.id;
 
-            // $td.appendChild($btn);
+            $btn.addEventListener('click', ()=>{
+                this.$tbody.innerHTML = '';
 
-            $tfooter.appendChild($btn);
-            // $tfooter.appendChild($btn)
-        })
+                let pageNumber = $btn.innerText;
+                let start = (pageNumber - 1) * this.dataCount;
+                let end = start + this.dataCount;
+                let forRender = this.data.slice(start, end);
+
+                this.renderData(this.dataCount, forRender);
+            });
+            
+            $td.appendChild($btn);
+            $btn.innerHTML = btnCount;
+            $tfooter.appendChild($td);
+
+        }
+
         this.$table.appendChild($tfooter);
     }
 }
